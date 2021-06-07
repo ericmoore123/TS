c82e00e21e63328315710f10470ac4a10d2ad653
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-06-06 23:29:58 -0400

    ts setup

---
+++ index.ts
@@ -0,0 +1,12 @@
+// TypeScript
+
+const birthdayGreeter = (name: string, age: number): string => {
+    return `Happy Birthday ${name}, you are now ${age} years old!`;
+};
+
+const user = 'John Doe';
+const age = 22; 
+console.log(birthdayGreeter(user, age));
+
+
+
