1ea905da2e822ce2b243297fe9a5cb2509966790
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-06-07 17:14:34 -0400

    ts basics intro

--- index.ts
+++ index.ts
@@ -13,10 +13,27 @@ const user_basic = {
 // Explicit type setting for objects
 interface User{
     name: string,
-    age: number
+    age: number,
+    id: number
 };
 
 const user_ts: User = { //If you assign object parameters other than those defined in 'interface User', an error will be thrown
     name: "John",
-    age: 20
+    age: 20,
+    id: 1
 };
+
+// TS fully support oop within the JS ecosystem and dynamic programming width types within classes
+class createUser{
+    name: string;
+    age: number;
+    id: number;
+
+    constructor(name: string, age: number, id: number){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    };
+};
+
+const newUser: User = new createUser('Charles', 35, 1)
