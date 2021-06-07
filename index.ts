6ef8522f802fdd1caccc07544629e5d4f08a718a
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-06-07 17:08:36 -0400

    ts basics intro

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
-// TypeScript
+// TypeScript stands in an unusual relationship to JavaScript. TypeScript offers all of JavaScript’s features, and an additional layer on top of these: 
+// TypeScript’s type system.
 
-const birthdayGreeter = (name: string, age: number): string => {
-    return `Happy Birthday ${name}, you are now ${age} years old!`;
+
+let firstType = "John Doe"; // On hover type: string
+
+// Inferring Object parameter Type
+const user_basic = {
+    name: "John",
+    age: 20
 };
 
-const user = 'John Doe';
-const age = 22; 
-console.log(birthdayGreeter(user, age));
+// Explicit type setting for objects
+interface User{
+    name: string,
+    age: number
+};
 
-
-
+const user_ts: User = { //If you assign object parameters other than those defined in 'interface User', an error will be thrown
+    name: "John",
+    age: 20
+};
