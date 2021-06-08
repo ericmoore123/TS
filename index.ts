2618f5cd10e833d04217b1db5bd3bd668354dc81
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-06-07 22:18:59 -0400

    Generics and Unions additions

--- index.ts
+++ index.ts
@@ -35,5 +35,44 @@ class createUser{
         this.id = id;
     };
 };
+const newUser: User = new createUser('Charles', 35, 1);
 
-const newUser: User = new createUser('Charles', 35, 1)
+// CUSTOM TYPES (Unions or Generics)
+// UNIONS
+type MyBool = true | false;
+
+// A popular use-case for union types is to describe the set of string or number literals that a value is allowed to be:
+type WindowStates = "open" | "closed" | "minimized";
+type LockStates = "locked" | "unlocked";
+type OddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+// Unions provide a way to handle different types too. For example, you may have a function that takes an array or a string:
+const getLength = (word: string | string[]) => word.length; // Accepts parameter of type string or array
+
+// You can make a function return different values depending on whether it is passed a string or an array or strings:
+function wrapInArray(param: string | string[]) {
+    if (typeof param === "string") {
+        console.log([param]);
+        return [param];
+    } else {
+      return param;
+    };
+};
+
+// GENERICS
+// Generics provide variables to types. A common example is an array. An array without generics could contain anything. 
+// An array with generics can describe the values that the array contains.
+
+type StringArray = Array<string>; //Array of strings
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+// Declare your own types as generics
+interface Backpack<Type> { //accepts a type as param
+    add: (obj: Type) => void;
+    get: () => Type;
+};
+declare const backpack: Backpack<number>; //declare "backpack" to use "interface Backpack" type of number
+
+const object = backpack.get(); // will return type of "number"
+// backpack.add("apples");  === THROWS TYPE ERROR ===
